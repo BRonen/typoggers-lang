5943db09eab264dde0453331a1ca1fdbb467f8fa
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-06 06:56:38 -0300

    feat(main): adding type checking layer

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,13 @@ module Main (main) where
 
 import Parser (parse)
 import Lexer (lexer)
+import Checker (typeCheck)
 
 main :: IO ()
-main = print $ parse $ (lexer "let a = 2 + 2 in a - 1")
+main = do
+    let ast = parse $ (lexer "let a: A = 2 + 2 in 2 - 1")
+    case typeCheck ast of
+        Right ast' -> print ast'
+        Left err -> do
+            print ast
+            print err
