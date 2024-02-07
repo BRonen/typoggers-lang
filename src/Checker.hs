9d3ca954f860c7c49488816ccc4ed2c768cf15b3
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-06 22:48:17 -0300

    feat: adding type checking context and variable checking on low and high terms

--- src/Checker.hs
+++ src/Checker.hs
@@ -1,5 +1,6 @@
-module Checker (typeCheck) where
+module Checker (checker) where
 
+import Data.Map as Map
 import Parser (
     Expr (..),
     TypeDef (..),
@@ -10,111 +11,159 @@ import Parser (
     Factor (..)
     )
 
--- TODO: Add variable type checking using a type context
+type Context = Map String String
 
-typeCheck :: Expr -> Either String Expr
-typeCheck (TypeDef typedef) = do
-    typedef' <- typeCheckTypeDef typedef
+checker :: Expr -> Either String Expr
+checker = typeCheck Map.empty
+
+typeCheck :: Context -> Expr -> Either String Expr
+typeCheck ctx (TypeDef typedef) = do
+    typedef' <- typeCheckTypeDef ctx typedef
     pure $ TypeDef typedef'
-typeCheck (Let name vartype value next) = do
-    value' <- typeCheck value
-    next' <- typeCheck next
+typeCheck ctx (Let name vartype value next) = do
+    let ctx' = Map.insert name vartype ctx
+    value' <- typeCheck ctx value
+    next' <- typeCheck ctx' next
     pure $ Let name vartype value' next'
 
-typeCheckTypeDef :: TypeDef -> Either String TypeDef
-typeCheckTypeDef (FuncDef funcdef) = do
-    funcdef' <- typeCheckFuncDef funcdef
+typeCheckTypeDef :: Context -> TypeDef -> Either String TypeDef
+typeCheckTypeDef ctx (FuncDef funcdef) = do
+    funcdef' <- typeCheckFuncDef ctx funcdef
     pure $ FuncDef funcdef'
-typeCheckTypeDef (Type name value next) = do
-    next' <- typeCheck next
+typeCheckTypeDef ctx (Type name value next) = do
+    next' <- typeCheck ctx next
     pure $ Type name value next'
 
-typeCheckFuncDef :: FuncDef -> Either String FuncDef
-typeCheckFuncDef (FuncApp funcapp) = do
-    funcapp' <- typeCheckFuncApp funcapp
+typeCheckFuncDef :: Context -> FuncDef -> Either String FuncDef
+typeCheckFuncDef ctx (FuncApp funcapp) = do
+    funcapp' <- typeCheckFuncApp ctx funcapp
     pure $ FuncApp funcapp'
-typeCheckFuncDef (Def param paramT retT body) = do
-    body' <- typeCheck body
+typeCheckFuncDef ctx (Def param paramT retT body) = do
+    body' <- typeCheck ctx body
     pure $ Def param paramT retT body'
 
-typeCheckFuncApp :: FuncApp -> Either String FuncApp
-typeCheckFuncApp (LowTerm lowterm) = do
-    lowterm' <- typeCheckLowTerm lowterm
+typeCheckFuncApp :: Context -> FuncApp -> Either String FuncApp
+typeCheckFuncApp ctx (LowTerm lowterm) = do
+    lowterm' <- typeCheckLowTerm ctx lowterm
     pure $ LowTerm lowterm'
-typeCheckFuncApp (App name param) = do
-    param' <- typeCheck param
+typeCheckFuncApp ctx (App name param) = do
+    param' <- typeCheck ctx param
     pure $ App name param'
 
-typeCheckLowTerm :: LowTerm -> Either String LowTerm
-typeCheckLowTerm (HighTerm highterm) = do
-    highterm' <- typeCheckHighTerm highterm
+typeCheckLowTerm :: Context -> LowTerm -> Either String LowTerm
+typeCheckLowTerm ctx (HighTerm highterm) = do
+    highterm' <- typeCheckHighTerm ctx highterm
     pure $ HighTerm highterm'
-typeCheckLowTerm (Plus x y) = do
+typeCheckLowTerm ctx (Plus x y) = do
     x' <- case x of
         HighTerm (Factor (Int x')) -> do
-            x'' <- typeCheckHighTerm $ Factor $ Int x'
+            x'' <- typeCheckHighTerm ctx $ Factor $ Int x'
             pure $ HighTerm x''
+        HighTerm (Factor (Name x')) -> do
+            case Map.lookup x' ctx of
+                Just "Int" -> do
+                    x'' <- typeCheckHighTerm ctx $ Factor $ Name x'
+                    pure $ HighTerm x''
+                _ -> Left "Trying to execute a Plus with a non-integer variable"
         HighTerm (Factor _) -> Left "Trying to execute a Plus with a non-integer value"
-        _ -> typeCheckLowTerm x
+        _ -> typeCheckLowTerm ctx x
     y' <- case y of
         Factor (Int _) -> do
-            y' <- typeCheckHighTerm y
+            y' <- typeCheckHighTerm ctx y
             pure y'
+        Factor (Name y') -> do
+            case Map.lookup y' ctx of
+                Just "Int" -> do
+                    pure $ Factor $ Name y'
+                _ -> Left "Trying to execute a Plus with a non-integer variable"
         Factor _ -> Left "Trying to execute a Plus with a non-integer value"
         _ -> do
-            y' <- typeCheckHighTerm y
+            y' <- typeCheckHighTerm ctx y
             pure y'
     pure $ Plus x' y'
-typeCheckLowTerm (Minus x y) = do
+typeCheckLowTerm ctx (Minus x y) = do
     x' <- case x of
         HighTerm (Factor (Int x')) -> do
-            x'' <- typeCheckHighTerm $ Factor $ Int x'
+            x'' <- typeCheckHighTerm ctx $ Factor $ Int x'
             pure $ HighTerm x''
+        HighTerm (Factor (Name x')) -> do
+            case Map.lookup x' ctx of
+                Just "Int" -> do
+                    x'' <- typeCheckHighTerm ctx $ Factor $ Name x'
+                    pure $ HighTerm x''
+                _ -> Left "Trying to execute a Minus with a non-integer variable"
         HighTerm (Factor _) -> Left "Trying to execute a Minus with a non-integer value"
-        _ -> typeCheckLowTerm x
+        _ -> typeCheckLowTerm ctx x
     y' <- case y of
         Factor (Int _) -> do
-            y' <- typeCheckHighTerm y
+            y' <- typeCheckHighTerm ctx y
             pure y'
+        Factor (Name y') -> do
+            case Map.lookup y' ctx of
+                Just "Int" -> do
+                    pure $ Factor $ Name y'
+                _ -> Left "Trying to execute a Minus with a non-integer variable"
         Factor _ -> Left "Trying to execute a Minus with a non-integer value"
         _ -> do
-            y' <- typeCheckHighTerm y
+            y' <- typeCheckHighTerm ctx y
             pure y'
     pure $ Minus x' y'
  
-typeCheckHighTerm :: HighTerm -> Either String HighTerm
-typeCheckHighTerm (Factor factor) = do
-    factor' <- typeCheckFactor factor
+typeCheckHighTerm :: Context -> HighTerm -> Either String HighTerm
+typeCheckHighTerm ctx (Factor factor) = do
+    factor' <- typeCheckFactor ctx factor
     pure $ Factor factor'
-typeCheckHighTerm (Div x y) = do
+typeCheckHighTerm ctx (Div x y) = do
     x' <- case x of
         Factor (Int x') -> do
-            x'' <- typeCheckFactor $ Int x'
+            x'' <- typeCheckFactor ctx $ Int x'
             pure $ Factor x''
+        Factor (Name x') -> do
+            case Map.lookup x' ctx of
+                Just "Int" -> do
+                    x'' <- typeCheckFactor ctx $ Name x'
+                    pure $ Factor x''
+                _ -> Left "Trying to execute a Division with a non-integer variable"
         Factor _ -> Left "Trying to execute a Division with a non-integer value"
-        _ -> typeCheckHighTerm x
+        _ -> typeCheckHighTerm ctx x
     y' <- case y of
         Int _ -> do
-            y' <- typeCheckFactor y
+            y' <- typeCheckFactor ctx y
             pure y'
+        Name y' -> do
+            case Map.lookup y' ctx of
+                Just "Int" -> do
+                    pure y
+                _ -> Left "Trying to execute a Division with a non-integer variable"
         _ -> Left "Trying to execute a Division with a non-integer value"
     pure $ Times x' y'
-typeCheckHighTerm (Times x y) = do
+typeCheckHighTerm ctx (Times x y) = do
     x' <- case x of
         Factor (Int x') -> do
-            x'' <- typeCheckFactor $ Int x'
+            x'' <- typeCheckFactor ctx $ Int x'
             pure $ Factor x''
+        Factor (Name x') -> do
+            case Map.lookup x' ctx of
+                Just "Int" -> do
+                    x'' <- typeCheckFactor ctx $ Name x'
+                    pure $ Factor x''
+                _ -> Left "Trying to execute a Multiplication with a non-integer variable"
         Factor _ -> Left "Trying to execute a Multiplication with a non-integer value"
-        _ -> typeCheckHighTerm x
+        _ -> typeCheckHighTerm ctx x
     y' <- case y of
         Int _ -> do
-            y' <- typeCheckFactor y
+            y' <- typeCheckFactor ctx y
             pure y'
+        Name y' -> do
+            case Map.lookup y' ctx of
+                Just "Int" -> do
+                    pure y
+                _ -> Left "Trying to execute a Multiplication with a non-integer variable"
         _ -> Left "Trying to execute a Multiplication with a non-integer value"
     pure $ Times x' y'
 
-typeCheckFactor :: Factor -> Either String Factor
-typeCheckFactor (Brack expr) = do
-    expr' <- typeCheck expr
+typeCheckFactor :: Context -> Factor -> Either String Factor
+typeCheckFactor ctx (Brack expr) = do
+    expr' <- typeCheck ctx expr
     pure $ Brack expr'
-typeCheckFactor a = Right a
+typeCheckFactor _ a = Right a
