5f6fdf453e39226ccc8cf618004666c0ee4b2407
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-07 02:25:14 -0300

    refactor: removing unused comments

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,6 @@ import Backend.Eval (eval)
 
 main :: IO ()
 main = do
-    -- let tokens = lexer "let a: Int = 2 in a * 1"
     let tokens = lexer "let a: Function = (x: Int): Int => x in let b: Int = 3 in let c: Bool = True in (c b)"
     let ast = parse tokens
     case checker ast of
