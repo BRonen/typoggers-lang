952984ad21a3fae1768e0598d93b5be6d1a75543
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-07 21:49:24 -0300

    tests: unit tests setup

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,16 @@
+module Main (main) where
+
+import qualified GoldenTests as GT
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  goldenTests <- GT.tests
+  defaultMain $ testGroup "Root" [goldenTests, test1, test2]
+
+test1 :: TestTree
+test1 = testCase "2+2=4" $ (2 + 2 :: Integer) @?= (4 :: Integer)
+
+test2 :: TestTree
+test2 = testCase "7 is even" $ assertBool "Oops, 7 is odd" (odd (7 :: Integer))
