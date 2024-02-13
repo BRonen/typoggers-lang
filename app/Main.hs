2438b1b402dcda4336e4f2d945c8289a699decbf
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-12 21:30:36 -0300

    fix: handling evaluation on main

--- app/Main.hs
+++ app/Main.hs
@@ -7,12 +7,15 @@ import Backend.Eval (eval)
 
 main :: IO ()
 main = do
-    let tokens = lexer "let a = if True then 1 else False in a"
+    let tokens = lexer "type A = Int in let a: Bool & A | Bool = if False then 1 else False in a"
     let ast = parse tokens
     case checker ast of
         Right resultT -> do
             print resultT
-            eval ast
+            result <- eval ast
+            case result of
+                Right v -> print v
+                Left err -> print err
         Left err -> do
             print ast
             print err
